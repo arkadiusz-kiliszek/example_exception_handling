ae31b593bb049da650293cb8d273cab01ecdcfd1
Arkadiusz Kiliszek <arkadiusz.kiliszek@gmail.com>
2023-07-23 15:32:37 +0200

    Dodanie customerDto oraz dependecies do walidajci

--- src/main/java/com/example/example_exception_handling/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/example/example_exception_handling/handlers/GlobalExceptionHandler.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value
-            = Exception.class)
+            = NoSuchCustomerExistsException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public @ResponseBody ErrorResponse handleException(Exception ex) {
+    public @ResponseBody ErrorResponse handleException(NoSuchCustomerExistsException ex) {
         return new ErrorResponse(
                 HttpStatus.NOT_FOUND.value(), ex.getMessage());
     }
